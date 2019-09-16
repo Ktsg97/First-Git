98c8df6994ad9a8fc3316bd3afcc751dcc42ce32
Ktsg97 <Selvaganesh.kt@outlook.com>
2019-09-16 11:55:22 +0530

    16/9

--- complex.cpp
+++ complex.cpp
@@ -1,31 +1,32 @@
 #include<iostream>
+#include<string.h>
 using namespace std;
 class complex
 {
  private:
-	int real,imag;
+	string s1,s2;
  public:
-	complex(int r=0,int i=0)
+	complex(string s3="ABC"/*string s4="XYZ"*/)
 	{
-		real = r;
-		imag = i;
+		s1 = s3;
+		//s2 = s4;
 	}
 	complex operator + (complex const &obj)
 	{
 		complex res;
-		res.real = real + obj.real;
-		res.imag = imag + obj.imag;
+		res.s1 = s1 +" "+obj.s1;
+		//res.s2 = s2 + obj.s2;
 		return res;
 	}
 	void print()
 	{
-		cout<<real<<"+ i"<<imag<<endl;
+		cout<<s1;
 	}
 };
 
 int main()
 {
- complex a(1,1),b(2,4);
+ complex a("Selva"),b("ganesh"); 
  complex c = a+b;
  c.print();
  return 0;
