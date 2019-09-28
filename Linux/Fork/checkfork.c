9504aa9fe232d16d1163fbd179b55378752a6c5d
Ktsg97 <Selvaganesh.kt@outlook.com>
2019-09-28 11:45:40 +0530

    Fork

---
+++ Linux/Fork/checkfork.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main()
+{
+ int cpid = fork();
+ if(cpid == 0)
+ {
+	while(1)
+	{
+		putchar('x');
+ 	}
+  }
+  else
+  {
+ 	while(1)
+	{
+		putc('0',stdout);
+	}
+  }
+  
+  return 0;
+}
+ 
